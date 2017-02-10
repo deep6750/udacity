8d1f115bfb7ca1ada5876edfbda0ebd063cefa22
Akash Deep Mishra <deep.mishra86@hotmail.com>
2017-02-11 01:55:22 +0530

    Update entertainment_center.py

--- project1/entertainment_center.py
+++ project1/entertainment_center.py
@@ -2,7 +2,7 @@ import media
 import fresh_tomatoes
 import urllib
 
-#movie title , storyline , movie poster, trailer url is passed in Movie contructor
+# movie title , storyline , movie poster, trailer url is passed in Movie contructor
 
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that comes to life",
@@ -33,7 +33,9 @@ hunger_games = media.Movie("Hunger Games",
                            "A really real reality show",
                            "HungerGamesPoster.jpg",
                            "https://www.youtube.com/watch?v=PbA63a7H0bo")
-#array of name movies is created and instance of class Movie is stored in array
+# Uses list of movie instances as input to generate an HTML file
+# and open it in the browser.
 movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+# This function opens a page of movies generated from the input, an array of movie instances.
 fresh_tomatoes.open_movies_page(movies)
 
